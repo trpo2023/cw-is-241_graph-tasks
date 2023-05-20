2d9821c13514abe0ab2669ed92cd21dc88ec0179
Jumkot <jumkot@mail.ru>
2023-05-20 22:41:40 +0700

    Upgrade main.c, #13

--- src/graph/main.c
+++ src/graph/main.c
@@ -46,6 +46,11 @@ int main()
         aim = how_many_paths(start - 1, end - 1, graph, aim);
         // Выводим результат работы функций
         output(choice, aim, NULL, NULL);
+        // Очистка памяти
+        for (int i = 0; i < aim->number; i++) {
+            free(aim->all_paths[i]);
+        }
+        free(aim);
         break;
     case '2':;
         Clover* kr = (Clover*)malloc(sizeof(Clover));
@@ -59,21 +64,30 @@ int main()
         kr = krPut(graph, start, end, kr);
         //Вывод рузультат работы функции
         output(choice, NULL, kr, NULL);
+        // Очистка памяти
+        free(kr->ver);
+        free(kr);
         break;
     case '3':;
         Roads* target = (Roads*)malloc(sizeof(Roads));
         target->path = malloc(MAX_SIZE * sizeof(int));
+        if (!target->path) {
+            free(target->path);
+        }
         target->path_dist = 0;
         // Запускаем поиск путей
         target = which_longer(start - 1, end - 1, graph, target);
         // Вывод результат работы функций
         output(choice, NULL, NULL, target);
+        // Очистка памяти
+        free(target->path);
+        free(target);
         break;
     default:
         printf("Выбранный номер задачи находится вне диапазона\n");
         break;
     }
-
+    // Очистка памяти
     for (int i = 0; i < graph->matrix_size; i++) {
         free(graph->matrix[i]);
     }
